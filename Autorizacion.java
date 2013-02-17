800b4725ef28e4e19248b109d9bfb0527e9de761
GeekyTheory <info@geekytheory.com>
2013-02-17 01:14:23 +0100

    Update Autorizacion.java

--- Autorizacion.java
+++ Autorizacion.java
@@ -15,16 +15,16 @@ import twitter4j.auth.RequestToken;
 import twitter4j.conf.ConfigurationBuilder;
 
 /**
- *
- * @author Mario Pérez
- *
- */
+*
+* @author Mario Pérez
+*
+*/
 public class Autorizacion {
     Autorizacion() throws IOException, TwitterException { //Constructor de la clase
         ConfigurationBuilder configBuilder = new ConfigurationBuilder();
-        configBuilder.setDebugEnabled(true)
-                .setOAuthConsumerKey("XXXXXXXXXXXX")
-                .setOAuthConsumerSecret("XXXXXXXXXXXXXX");
+            configBuilder.setDebugEnabled(true)
+                    .setOAuthConsumerKey(new Tokens().OAuthConsumerKey)
+                    .setOAuthConsumerSecret(new Tokens().OAuthConsumerSecret);
         Twitter OAuthTwitter = new TwitterFactory(configBuilder.build()).getInstance();
         RequestToken requestToken = null;
         AccessToken accessToken = null;
@@ -33,23 +33,22 @@ public class Autorizacion {
             try {
                 requestToken = OAuthTwitter.getOAuthRequestToken();
                 System.out.println("Request Tokens obtenidos con éxito.");
-                System.out.println("Request Token: " + requestToken.getToken());
-                System.out.println("Request Token secret: " + requestToken.getTokenSecret());
+                /*System.out.println("Request Token: " + requestToken.getToken());
+                System.out.println("Request Token secret: " + requestToken.getTokenSecret());*/
                 url = requestToken.getAuthorizationURL();
-                System.out.println("URL:");
-                System.out.println(requestToken.getAuthorizationURL());
             } catch (TwitterException ex) {
                 Logger.getLogger(TwitterJavaGT.class.getName()).log(Level.SEVERE, null, ex);
             }
             BufferedReader lectorTeclado = new BufferedReader(new InputStreamReader(System.in));
-            //Abro el navegador. Firefox, en este caso.
+            //Abro el navegador.
+
             Runtime runtime = Runtime.getRuntime();
             try {
                 runtime.exec("firefox " + url);
             } catch (Exception e) {
             }
             //Nos avisa de que introduciremos el PIN a continuación
-            System.out.print("Introduce el PIN del navegador y pulsa intro.\n\n      PIN: ");
+            System.out.print("Introduce el PIN del navegador y pulsa intro.\n\n PIN: ");
             //Leemos el PIN
             String pin = lectorTeclado.readLine();
 
@@ -58,15 +57,17 @@ public class Autorizacion {
             } else {
                 accessToken = OAuthTwitter.getOAuthAccessToken(requestToken);
             }
+            
         } while (accessToken == null);
+        
         System.out.println("\n\nAccess Tokens obtenidos con éxito.");
-        System.out.println("Access Token: " + accessToken.getToken());
-        System.out.println("Access Token secret: " + accessToken.getTokenSecret());
+        /*System.out.println("Access Token: " + accessToken.getToken());
+        System.out.println("Access Token secret: " + accessToken.getTokenSecret());*/
         FileOutputStream fileOS = null;
         File file;
         String content = accessToken.getToken() + "\n" + accessToken.getTokenSecret();
         try {
-            file = new File("/home/mario/auth_file.txt");
+            file = new File(System.getProperty("user.home")+"/auth_file.txt".replace("\\","/"));
             fileOS = new FileOutputStream(file);
             //Si el archivo no existe, se crea
             if (!file.exists()) {
@@ -88,6 +89,7 @@ public class Autorizacion {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+            TwitterJavaGT cliente=new TwitterJavaGT();
         }
     }
 }
